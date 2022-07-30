92b6f89a6fe025bcd2e7dc5a1873fc61af1a2b75
Alejandro <alejandroaps93@gmail.com>
2022-07-30 13:52:48 +0200

    Partidos cuartos de final Parte 1

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ const grupoC = ["Alemania", "Islandia"];
 const grupoD = ["Francia", "Suiza"];
 
 
+class match
+{
+    constructor(team1,team2)
+    {
+        this.team1 = team1;
+        this.team2 = team2;
+        this.goalsT1 = -1;
+        this.goalsT2 = -1;
+        this.teamWinner = null;
+        this.teamLoser = null;
+    }
+
+    toString()
+    {
+        return this.team1 +  " " + this.goalsT1 + " - " + this.goalsT2 +  " " + this.team2 + " => " + this.teamWinner;
+    }
+
+}
+
 
 // Program
 console.log("====================================================");
@@ -19,3 +38,14 @@ console.log("GRUPO B:",grupoB[0], grupoB[1]);
 console.log("GRUPO C:",grupoC[0], grupoC[1]);
 console.log("GRUPO D:",grupoD[0], grupoD[1]);
 
+console.log("\n===== CUARTOS DE FINAL =====\n");
+
+const cuartos1 = new match(grupoA[0],grupoB[1]);
+const cuartos2 = new match(grupoB[0],grupoA[1]);
+const cuartos3 = new match(grupoC[0],grupoD[1]);
+const cuartos4 = new match(grupoD[0],grupoC[1]);
+
+console.log(cuartos1.toString());
+console.log(cuartos2.toString());
+console.log(cuartos3.toString());
+console.log(cuartos4.toString());
