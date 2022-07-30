16cb4732786580a921ca1798594177b9991517a1
Alejandro <alejandroaps93@gmail.com>
2022-07-30 13:34:38 +0200

    Requisito 2: Mostrar los 8 equipos participantes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-
+// teams 
+                // 1º      2º
+const grupoA = ["España", "Noruega"];
+const grupoB = ["Inglaterra", "Países Bajos"];
+const grupoC = ["Alemania", "Islandia"];
+const grupoD = ["Francia", "Suiza"];
 
 
 
@@ -6,3 +11,11 @@
 console.log("====================================================");
 console.log("=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===");
 console.log("====================================================");
+
+console.log("\nEquipos que participan en el playoff:\n")
+
+console.log("GRUPO A:",grupoA[0], grupoA[1]);
+console.log("GRUPO B:",grupoB[0], grupoB[1]);
+console.log("GRUPO C:",grupoC[0], grupoC[1]);
+console.log("GRUPO D:",grupoD[0], grupoD[1]);
+
